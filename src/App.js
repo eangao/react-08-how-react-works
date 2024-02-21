eac897f1a47fded0ada8b50d1f07b53f677a9961
elmar <elmarcera@gmail.com>
2024-02-21 10:43:24 +0800

    Resetting State With the Key Prop

--- src/App.js
+++ src/App.js
@@ -39,7 +39,10 @@ function Tabbed({ content }) {
       </div>
 
       {activeTab <= 2 ? (
-        <TabContent item={content.at(activeTab)} />
+        <TabContent
+          item={content.at(activeTab)}
+          key={content.at(activeTab).summary}
+        />
       ) : (
         //         So this at here is basically modern JavaScript
         // which is the same as this here,
